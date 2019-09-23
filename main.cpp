2fbe0217f1bfe411af20b1bfd3a7cf89516a48f5
JennGriff <jgriffiths0101@gmail.com>
2019-09-23 15:27:01 -0700

    added burp message to chirp

--- main.cpp
+++ main.cpp
@@ -8,17 +8,21 @@ using std::string;
 int main()
 {
   string chirp;
+  string burp;
 
   do
   {
 
     cout<<"Please enter your Chirp:"<<endl;
     getline(cin,chirp);
+    cout<<"PLease enter your Burp"<<endl;
+    getline(cin, burp);
 
     if( chirp.length() > 100 )
     {
       cout<<"Sorry, Chirps must be under 100 characters.\n";
       cout<<"Yours was "<<chirp.length()-100<<" too long!\n";
+      cout<<burp<<endl;
     }
   }while( chirp.length() > 100 );
 
